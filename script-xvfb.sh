a44f8dedd6025b61556f68bdeef7b1f6954d336c
Xun Wilson Huang <xhuang@gmail.com>
2021-05-23 20:53:38 -0700

    save

--- script-xvfb.sh
+++ script-xvfb.sh
@@ -12,7 +12,8 @@ xterm &
 x11vnc -display $DISPLAY -bg -forever -nopw  -xkb & 
 sleep 3
 websockify --web /usr/share/novnc 8080 localhost:5900 &
-sleep 1
-lt --port 8080 | awk '{print $4."/vnc.html"}'     
+sleep 5
+lt --port 8080 
+# lt --port 8080 | awk '{print $4."/vnc.html"}'     
 sleep 3
 echo "done"
