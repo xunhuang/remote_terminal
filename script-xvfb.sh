ac21d858c0ef568e234cf8934db539fdc2a88aee
Xun Wilson Huang <xhuang@gmail.com>
2021-05-23 20:41:03 -0700

    fine tune

--- script-xvfb.sh
+++ script-xvfb.sh
@@ -1,16 +1,17 @@
 
 sudo apt-get update
 sudo apt-get install -y x11vnc xvfb fluxbox  websockify novnc xterm
-sudo npm install -g localtunnel
 
 export DISPLAY=:1
 Xvfb $DISPLAY -screen 0 1024x768x16 &
-fluxbox &
+
 sudo npm install -g localtunnel
+fluxbox &
+sleep 1
 xterm &
 x11vnc -display $DISPLAY -bg -forever -nopw  -xkb & 
 sleep 3
 websockify --web /usr/share/novnc 8080 localhost:5900 &
 sleep 1
 lt --port 8080 | awk '{print $4."/vnc.html"}'     &
-sleep 5
+sleep 3
