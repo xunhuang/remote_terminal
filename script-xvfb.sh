fe2960aa6934c43292125130c6055974b712c8f8
Xun Wilson Huang <xhuang@gmail.com>
2021-05-23 20:49:19 -0700

    hmm

--- script-xvfb.sh
+++ script-xvfb.sh
@@ -13,6 +13,6 @@ x11vnc -display $DISPLAY -bg -forever -nopw  -xkb &
 sleep 3
 websockify --web /usr/share/novnc 8080 localhost:5900 &
 sleep 1
-lt --port 8080 | awk '{print $4."/vnc.html"}'     &
+lt --port 8080 | awk '{print $4."/vnc.html"}'     
 sleep 3
 echo "done"
