d9e7c033af23f603a0488a9efd8d9537c7e66fa2
xunhuang <xhuang@gmail.com>
2024-07-10 11:25:25 -0700

    push change

--- script-xvfb.sh
+++ script-xvfb.sh
@@ -1,9 +1,17 @@
 
+
+curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc | \
+  sudo gpg --dearmor -o /etc/apt/keyrings/ngrok.gpg && \
+  echo "deb [signed-by=/etc/apt/keyrings/ngrok.gpg] https://ngrok-agent.s3.amazonaws.com buster main" | \
+  sudo tee /etc/apt/sources.list.d/ngrok.list && \
+  sudo apt update && sudo apt install ngrok
+ngrok config add-authtoken $NGROK_TOKEN
+
 sudo apt-get update
 sudo apt-get install -y x11vnc xvfb fluxbox  websockify novnc xterm
 
 export DISPLAY=:1
-Xvfb $DISPLAY -screen 0 1024x768x16 &
+Xvfb $DISPLAY -screen 0 1600x1200x16 &
 
 sudo npm install -g localtunnel
 fluxbox &
@@ -13,7 +21,10 @@ x11vnc -display $DISPLAY -bg -forever -nopw  -xkb &
 sleep 3
 websockify --web /usr/share/novnc 8080 localhost:5900 &
 sleep 5
-lt --port 8080  &
+# lt --port 8080  &
 # lt --port 8080 | awk '{print $4."/vnc.html"}'      &
 sleep 5
-echo "Done with this... hopefully we see a port"
+# echo "Done with this... hopefully we see a port"
+ngrok http http://localhost:8080  &
+# should vist whateversite_from_ngrok/vnc.html
+
